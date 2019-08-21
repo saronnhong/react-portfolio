dfe03f333263192ee645189e23365f31fb6b385b
Saron <saronnhong@gmail.com>
2019-08-21 11:25:45 -0700

    code clean up, remove axios from packagejson, remove nodemailer code, working on heroku

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ const app = express();
 
 const cors = require('cors'); //needed to disable sendgrid security
 
-
 app.use(cors()); //utilize Cors so the browser doesn't restrict data, without it Sendgrid will not send!
-
-
-// const nodemailer = require('nodemailer');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -26,43 +23,16 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.post("/send-email", function(req, res) {
   
-  
   const msg = {
     to: req.body.email.sender,
     from: req.body.email.recipient,
     subject: req.body.email.subject,
-    text: 'and easy to do anywhere, even with Node.js',
-    html: "FROM: " + req.body.email.from + "\n\n" + req.body.email.text,
+    text: '',
+    html: "FROM: " + req.body.email.from + "\n" + req.body.email.text,
   };
   sgMail.send(msg)
   .then((msg) => console.log('sendGrid email sent!'));
 })
-
-// Define API routes here
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//       user: process.env.EMAIL,
-//       pass: process.env.PASSWORD
-//   }
-// });
-
-// app.post("/send-email", function(req, res) {
-//   let mailOptions = {
-//     from: req.body.email.recipient,
-//     to: req.body.email.sender,
-//     subject: req.body.email.subject,
-//     text: "FROM: " + req.body.email.from + "\n\n" + req.body.email.text
-//   };
-
-//   transporter.sendMail(mailOptions, function (err, data) {
-//       if (err) {
-//           console.log("error occurs ", err);
-//       } else {
-//           console.log("email sent!");
-//       }
-//   });
-// })
 
 // Send every other request to the React app
 // Define any API routes before this runs
