d58a03d9291fcb638d373aec404cf088a1c1e4dd
Saron <saronnhong@gmail.com>
2019-08-21 08:10:37 -0700

    add server secret and run build

---
+++ client/build/precache-manifest.2e612eeef075f7f4ca871fe8e826c6d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec52da5a88824e8c52c508acbf6a530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86c16f57fad37294e930",
+    "url": "/static/css/main.e767e89a.chunk.css"
+  },
+  {
+    "revision": "91fe2aa5a9d2ee1e9d26",
+    "url": "/static/js/2.5c78e5de.chunk.js"
+  },
+  {
+    "revision": "86c16f57fad37294e930",
+    "url": "/static/js/main.b100fe48.chunk.js"
+  },
+  {
+    "revision": "cd04e7dca758dcdd3c69",
+    "url": "/static/js/runtime~main.27c87217.js"
+  }
+]);
