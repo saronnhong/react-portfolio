d4a7658fde91c389428b13459f927b99978b77bc
Saron <saronnhong@gmail.com>
2019-08-22 22:31:06 -0700

    remove modal test

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -6,58 +6,63 @@ import "./style.css";
 
 
 
-class Portfolio extends Component  {
+class Portfolio extends Component {
+
     componentDidMount() {
-        window.scrollTo(0,0);
-      }
-    render(){
-    return (
-        <div className="portMain animated bounceInRight slower">
-            <div className="container portContainer">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h1 id="portfolioTitle">My Portfolio</h1>
-                    </div>
-                </div>
+        window.scrollTo(0, 0);
+    }
 
-                {Project.map((projects) =>
+    render() {
+        return (
+            <div className="portMain animated bounceInRight slower">
+                <div className="container portContainer">
                     <div className="row">
-                        <div className="col-md-6 cardRow">
-                            <div className="card cardPort">
-                                <a href={projects.deployLink1}>
-                                    <img src={process.env.PUBLIC_URL + projects.imageLocation1} className="card-img-top" alt="..." />
-                                </a>
-                                <div className="card-body">
-                                    <div className="card-text">{projects.project1}
-                                        <a href={projects.githubLink1}>
-                                            <i className="fab fa-github githubFA"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-md-6 cardRow">
-                            <div className="card cardPort">
-                                
-                                <a href={projects.deployLink2}>
-                                    <img width="100%" src={process.env.PUBLIC_URL + projects.imageLocation2} className="card-img-top" alt="..." />
-                                </a>
-                                <div className="card-body">
-                                    <div className="card-text">{projects.project2}
-                                    
-                                        <a href={projects.githubLink2}>
-                                        <i className="fab fa-github githubFA"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="col-md-12">
+                            <h1 id="portfolioTitle">My Portfolio</h1>
                         </div>
                     </div>
-                )}
-            </div>
-        </div >
-    );
-}
+
+                    {Project.map((projects) =>
+                        <div className="row">
+                            <div className="col-md-6 cardRow">
+
+                                <div className="card cardPort">
+                                    <a href={projects.deployLink1} >
+                                        <img src={process.env.PUBLIC_URL + projects.imageLocation1} className="card-img-top" alt="..." />
+                                    </a>
+                                    <div className="card-body">
+
+                                        <div className="card-text">{projects.project1}
+                                            <a href={projects.githubLink1}>
+                                                <i className="fab fa-github githubFA"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className="col-md-6 cardRow">
+                                <div className="card cardPort">
+
+                                    <a href={projects.deployLink2}>
+                                        <img width="100%" src={process.env.PUBLIC_URL + projects.imageLocation2} className="card-img-top" alt="..." />
+                                    </a>
+                                    <div className="card-body">
+                                        <div className="card-text">{projects.project2}
+
+                                            <a href={projects.githubLink2}>
+                                                <i className="fab fa-github githubFA"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div >
+        );
+    }
 }
 export default Portfolio;
