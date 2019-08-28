f27aac01d8894d66a04fed39dd29f69e6caf4f39
Saron <saronnhong@gmail.com>
2019-08-27 17:28:37 -0700

    add resume link

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import About from './components/About';
+// import Resume from "process.env.PUBLIC_URL + '/myfile.pdf'"
 
 ReactDOM.render(
     <Router>
@@ -19,6 +20,7 @@ ReactDOM.render(
                 <Route exact path="/about" component={About} />
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/contact" component={Contact} />
+                {/* <Route exact path="/resume" component={Resume} /> */}
                 <Route component={App} />
             </Switch>
 
